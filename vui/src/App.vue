93cf8fe4326f19afb180d46baec9c7df51daf156
Aidar Uldanov <root@sunsongs.ru>
2019-11-04 22:06:40 +0100

    App component as main entry point. CSRF token is stored in vuex repo on initializing the app

---
+++ vui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container" id="app">
+        <nav-bar></nav-bar>
+        <router-view/>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+    import NavBar from "./components/NavBar";
+    import {mapActions} from 'vuex'
+
+    export default {
+        components: {NavBar},
+        methods: {
+            ...mapActions('csrf', ['setToken']),
+        },
+        created: function () {
+            const bodyHtmlTag = document.getElementsByTagName("body")[0];
+            const csrfToken = bodyHtmlTag.getAttribute('csrf-token-value');
+            this.setToken(csrfToken);
+        },
+    }
+</script>
