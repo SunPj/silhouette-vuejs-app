496a1ed018bf4e20102ae0919a97621956bee29e
Aidar Uldanov <root@sunsongs.ru>
2020-01-10 20:41:30 +0100

    Recaptcha integration on backend

--- app/modules/BaseModule.scala
+++ app/modules/BaseModule.scala
@@ -2,6 +2,7 @@ package modules
 
 import com.google.inject.{AbstractModule, Provides}
 import javax.inject.Named
+import models.services.captcha.{CaptchaService, ReCaptchaConfig, ReCaptchaService}
 import models.services.{AuthTokenService, AuthTokenServiceImpl}
 import net.codingwell.scalaguice.ScalaModule
 import play.api.Configuration
@@ -16,11 +17,17 @@ class BaseModule extends AbstractModule with ScalaModule {
     */
   override def configure(): Unit = {
     bind[AuthTokenService].to[AuthTokenServiceImpl]
+    bind[CaptchaService].to[ReCaptchaService]
   }
 
   @Named("SendGridApiKey")
   @Provides
   def providesSendGridApiKey(conf: Configuration): String = {
     conf.get[String]("sendgrid.api.key")
+  }
+
+  @Provides
+  def providesReCaptchaConfig(conf: Configuration): ReCaptchaConfig = {
+    ReCaptchaConfig(conf.get[String]("recaptcha.secretKey"))
   }
 }
